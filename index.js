eb834b4d527e85c04a8d284eca3be910c056836e
sameerbhagtani <bhagtanisameer@gmail.com>
2026-06-15 06:46:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.get("/", (req, res) => {
+    return res.json({ message: "Server is healthy" });
+});
+
+app.listen(PORT, () => {
+    console.log(`✅ Server started at PORT: ${PORT}`);
+});
