08c80223ac71830796fbd22c5f3ccab0f5264378
sameerbhagtani <bhagtanisameer@gmail.com>
2026-06-16 03:04:11 +0530

    minor change for testing

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.get("/", (req, res) => {
-    return res.json({ message: "Server is healthy (github actions)" });
+    return res.json({
+        message: "Server is healthy (github actions) docker 🐋",
+    });
 });
 
 app.listen(PORT, () => {
