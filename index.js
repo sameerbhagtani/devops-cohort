1f6b2895649e145a4ce1792b42229ff193a36c3d
sameerbhagtani <bhagtanisameer@gmail.com>
2026-06-16 02:15:48 +0530

    configure github actions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.get("/", (req, res) => {
-    return res.json({ message: "Server is healthy" });
+    return res.json({ message: "Server is healthy (github actions)" });
 });
 
 app.listen(PORT, () => {
