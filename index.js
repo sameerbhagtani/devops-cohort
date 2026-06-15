49d366d956ae79ba3607e29677f5300cc4922874
sameerbhagtani <bhagtanisameer@gmail.com>
2026-06-16 04:06:17 +0530

    minor change for ssl testing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT ?? 3000;
 
 app.get("/", (req, res) => {
     return res.json({
-        message: "Server is healthy (github actions) docker 🐋",
+        message: "Server is healthy (github actions) docker 🐋. SSL testing.",
     });
 });
 
